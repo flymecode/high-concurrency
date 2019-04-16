1b9cfe7f4df7643df32ca25abac0852c267f74d4
flymecode <1272193309@qq.com>
2019-04-16 15:51:20 +0800

    并发

---
+++ src/main/java/com/xupt/highconcurrency/amqp/RabbitMQServer.java
@@ -0,0 +1,9 @@
+package com.xupt.highconcurrency.amqp;
+
+/**
+ * @author maxu
+ * @date 2019/4/14
+ */
+public class RabbitMQServer {
+
+}
---
+++ src/main/java/com/xupt/highconcurrency/redis/RedisClient.java
@@ -0,0 +1,41 @@
+package com.xupt.highconcurrency.redis;
+
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import javax.annotation.Resource;
+
+/**
+ * @author maxu
+ * @date 2019/4/14
+ */
+@Component
+public class RedisClient {
+    @Resource(name = "redisPool")
+    private JedisPool jedisPool;
+
+    public void set(String key, String value) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            jedis.set(key, value);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public String get(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            return jedis.get(key);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+}
